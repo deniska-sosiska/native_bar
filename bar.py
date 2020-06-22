768f58fbe052f7b0a8b939b85bda9330ffe813e0
deniska-sosiska <denchikarabik@gmail.com>
2020-06-22 18:40:18 +0300

    Создал карточки на нужное количество игроков

--- bar.py
+++ bar.py
@@ -1,16 +1,18 @@
 import random
-cards = 10
+
+users = 10
+# users = int(input("Сколько человек будет играть?: "))
+cards = []
 
 
 #===================Создание катастрофы======================================
 with open('files/Катастрофы.txt') as  fileThatOpen:
-    fileThatOpen.seek(0)
     lines = 0
     for i in fileThatOpen:
         lines += 1
     needCatastr = (random.randrange(1, lines, 2))
     fileThatOpen.seek(0)
-    fileThatCreate = open('files/!Катастрофа.txt', 'w')
+    fileThatCreate = open('Карточки/!Катастрофа.txt', 'w')
     for i in range(lines):
         if (i + 1 == needCatastr):
             fileThatCreate.write('Катастрофа: ')
@@ -25,7 +27,7 @@ with open('files/Катастрофы.txt') as  fileThatOpen:
 # допол карты навыков
 # спец условия
 
-class card():
+class Card():
 
     def __init__(self, profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality):
         # self.profession = get_info("Профессии.txt")
@@ -37,14 +39,23 @@ class card():
         self.__phobias = phobias
         self.__humanQuality = humanQuality
 
-    def output(self):
-        print('Профессия:                     ',self.__profession, end = '')
-        print('Здоровье:                      ',self.__health, end = '')
-        print('Биологическая характерист-ка:  ',self.__biologicalCharacterization)
-        print('Багаж:                         ',self.__baggage, end = '')
-        print('Хобби:                         ',self.__hobby, end = '')
-        print('Фобии:                         ',self.__phobias, end = '')
-        print('Качество человека:             ',self.__humanQuality)
+    def output(self, numberUsers):
+        with open('Карточки/' + numberUsers + '.txt', 'w') as fileU:
+            fileU.write('Профессия:                     ')
+            fileU.write(self.__profession)
+            fileU.write('Здоровье:                      ')
+            fileU.write(self.__health)
+            fileU.write('Биологическая характерист-ка:  ')
+            fileU.write(str(self.__biologicalCharacterization))
+            fileU.write('\n')
+            fileU.write('Багаж:                         ')
+            fileU.write(self.__baggage)
+            fileU.write('Хобби:                         ')
+            fileU.write(self.__hobby)
+            fileU.write('Фобии:                         ')
+            fileU.write(self.__phobias)
+            fileU.write('Качество человека:             ')
+            fileU.write(self.__humanQuality)
 
 
 
@@ -54,11 +65,11 @@ def get_info(nameFile):
         lines = 0
         for i in fileThatOpen:
             lines += 1
-        needChar = (random.randint(0, lines))
+        needChar = (random.randint(1, lines))
         fileThatOpen.seek(0)
         # fileThatCreate = open('files/'+ nameFile+ '!' +'.txt', 'w')
         for i in range(lines):
-            if (i == needChar):
+            if (i + 1 == needChar):
                 attribute = fileThatOpen.readline()
                 # fileThatCreate.write(qwe)
             fileThatOpen.readline()
@@ -67,33 +78,43 @@ def get_info(nameFile):
 
 
 #============================получаю атрибуты================================
-profession = get_info("Профессии")
-health = get_info("Болезни")
-baggage = get_info("Багаж")
-hobby = get_info("Хобби")
-phobias = get_info("Фобии")
-humanQuality = get_info('Качество человека')
 
-#=====получаю пол
-forSex = random.randint(0, 1)
-if forSex == 1:
-    sex = 'Мужчина'
-else:
-    sex = 'Женщина'
-#=====получаю возраст
-age = random.randint(18, 89)
-#=====получаю чайлдфри
-forChildfree = random.randint(0, 4)
-if forChildfree == 4:
-    childfree = 'Чайлдфри'
-    biologicalCharacterization = [sex , age, childfree ]
-else:
-    biologicalCharacterization = [sex , age]
+for user in range(users):
+    profession = get_info("Профессии")
+    forHealth = random.randint(0, 3)
+    if (forHealth == 3):
+        health = 'Полностью здоров\n'
+    else:
+        health = get_info("Болезни")
+    baggage = get_info("Багаж")
+    hobby = get_info("Хобби")
+    forPhobias = random.randint(0, 3)
+    if (forPhobias == 3):
+        phobias = 'Фобий нету\n'
+    else:
+        phobias = get_info("Фобии")
+    humanQuality = get_info('Качество человека')
+    forSex = random.randint(0, 1)
+    if forSex == 1:
+        sex = 'Мужчина'
+    else:
+        sex = 'Женщина'
+    #=====получаю возраст
+    age = random.randint(18, 89)
+    #=====получаю чайлдфри
+    forChildfree = random.randint(0, 4)
+    if forChildfree == 4:
+        childfree = 'Чайлдфри'
+        biologicalCharacterization = [sex , age, childfree ]
+    else:
+        biologicalCharacterization = [sex , age]
 
 
-#====================================создаю класс====================================
-cards = card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality)
-cards.output()
+    card = Card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality)
+    card.output('user' + str(user + 1))
+
+
+
 
 
 
