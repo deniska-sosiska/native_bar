32d206e0e58266315cc514612b9c82cee2174c26
deniska-sosiska <denchikarabik@gmail.com>
2020-06-23 21:21:02 +0300

    Почти сделал неповторки.... А ушло на это 6 часов Жизнь-боль

--- bar.py
+++ bar.py
@@ -1,26 +1,45 @@
 import random
 
-# users = 10
-users = int(input("Сколько человек будет играть?: "))
-# cards = []
+users = 10
+# users = int(input("Сколько человек будет играть?: "))
+professionThatWas = []
+healthThatWas = []
+baggageThatWas = []
+hobbyThatWas = []
+phobiasThatWas = []
 
+# print(professionThatWas)
 
 #===================Создание катастрофы======================================
-with open('files/Катастрофы.txt') as  fileThatOpen:
-    lines = 0
-    for i in fileThatOpen:
-        lines += 1
-    needCatastr = (random.randrange(1, lines, 2))
-    fileThatOpen.seek(0)
-    fileThatCreate = open('Карточки/!Катастрофа.txt', 'w')
-    for i in range(lines):
-        if (i + 1 == needCatastr):
-            fileThatCreate.write('Катастрофа: ')
-            fileThatCreate.write(fileThatOpen.readline())
-            fileThatCreate.write('Описание: ')
-            fileThatCreate.write(fileThatOpen.readline())
-        fileThatOpen.readline()
-    fileThatCreate.close()
+def catastrophe():
+    with open('files/Катастрофы.txt') as  fileThatOpen:
+        exitFromCreateCatastrophe = True
+        while exitFromCreateCatastrophe:
+            fileThatOpen.seek(0)
+            lines = 0
+            for i in fileThatOpen:
+                lines += 1
+            needCatastr = (random.randrange(1, lines, 2))
+            # print(needCatastr)
+            fileThatOpen.seek(0)
+            fileThatChecking = open('Карточки/!Катастрофа.txt', 'r+')
+            catastropheThatWas = (fileThatChecking.readline())
+            fileThatChecking.seek(0)
+            for i in range(lines):
+                if (i + 1 == needCatastr):
+                    forNameCatastrophe = fileThatOpen.readline()
+                    forDescriptionCatastrophe = fileThatOpen.readline()
+                    if (catastropheThatWas == forNameCatastrophe):
+                        fileThatChecking.close()
+                    else:
+                        nameCatastrophe  = forNameCatastrophe
+                        descriptionCatastrophe = forDescriptionCatastrophe
+                        fileThatChecking.write(nameCatastrophe)
+                        fileThatChecking.write(descriptionCatastrophe)
+                        exitFromCreateCatastrophe = False
+                fileThatOpen.readline()
+
+    return nameCatastrophe, descriptionCatastrophe
 
 
 
@@ -29,7 +48,7 @@ with open('files/Катастрофы.txt') as  fileThatOpen:
 
 class Card():
 
-    def __init__(self, profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality):
+    def __init__(self, profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality, nameCatastrophe, descriptionCatastrophe):
         # self.profession = get_info("Профессии.txt")
         self.__profession = profession
         self.__health = health
@@ -41,6 +60,9 @@ class Card():
 
     def output(self, numberUsers):
         with open('Карточки/' + numberUsers + '.txt', 'w') as fileU:
+            fileU.write(nameCatastrophe)
+            fileU.write(descriptionCatastrophe)
+            fileU.write("\n\n")
             fileU.write('Профессия:                     ')
             fileU.write(self.__profession)
             fileU.write('Здоровье:                      ')
@@ -58,26 +80,61 @@ class Card():
             fileU.write(self.__humanQuality)
 
 
-
-
 def get_info(nameFile):
     with open('files/' + nameFile + '.txt', 'r') as fileThatOpen:
-        lines = 0
-        for i in fileThatOpen:
-            lines += 1
-        needChar = (random.randint(1, lines))
-        fileThatOpen.seek(0)
-        # fileThatCreate = open('files/'+ nameFile+ '!' +'.txt', 'w')
-        for i in range(lines):
-            if (i + 1 == needChar):
-                attribute = fileThatOpen.readline()
-                # fileThatCreate.write(qwe)
-            fileThatOpen.readline()
-        # fileThatCreate.close()
-        return attribute
+        exitFromCreateAtribute = True
+        while exitFromCreateAtribute:
+            fileThatOpen.seek(0)
+            lines = 0
+            for i in fileThatOpen:
+                lines += 1
+            needChar = (random.randint(1, lines))
+            fileThatOpen.seek(0)
+            for i in range(lines):
+                if (i + 1 == needChar):
+                    forAttribute = fileThatOpen.readline()
+                    if (nameFile == 'Профессии'):
+                        print(forAttribute,end='')
+                        professionThatWas.insert(0, forAttribute)
+                        # professionThatWas.append(0)
+                        x = 0
+                        for i in professionThatWas:
+                            x += 1
+                            if x == 1:
+                                continue
+                            else:
+                                if i  == forAttribute:
+                                    professionThatWas.pop(0)
+                                    # break
+                                else:
+                                    attribute = forAttribute
+                                    exitFromCreateAtribute = False
+
+                            # input(professionThatWas)
+
+                    else:
+                        attribute = forAttribute
+                        exitFromCreateAtribute = False
+
+                fileThatOpen.readline()
+
+    return attribute
+
+
+                    # if (nameFile == 'Болезни'):
+                    #     professionThatWas.append(forAttribute)
+                    # if (nameFile == 'Багаж'):
+                    #     professionThatWas.append(forAttribute)
+                    # if (nameFile == 'Хобби'):
+                    #     professionThatWas.append(forAttribute)
+                    # if (nameFile == 'Фобии'):
+                    #     professionThatWas.append(forAttribute)
+                    #     # print(professionThatWas)
 
 
 #============================получаю атрибуты================================
+nameCatastrophe, descriptionCatastrophe = catastrophe()
+
 
 for user in range(users):
     profession = get_info("Профессии")
@@ -100,7 +157,7 @@ for user in range(users):
     else:
         sex = 'Женщина'
     #=====получаю возраст
-    age = random.randint(18, 89)
+    age = random.randint(18, 63)
     #=====получаю чайлдфри
     forChildfree = random.randint(0, 4)
     if forChildfree == 4:
@@ -110,7 +167,7 @@ for user in range(users):
         biologicalCharacterization = [sex , age]
 
 
-    card = Card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality)
+    card = Card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality, nameCatastrophe, descriptionCatastrophe)
     card.output('user' + str(user + 1))
 
 
@@ -118,6 +175,7 @@ for user in range(users):
 
 
 
+print(professionThatWas)
 
 
 
